4507c86deeb286f8c503ea8a07ec6722bda36080
Thaís Silva <thaiscaroline.cms@gmail.com>
2022-01-20 19:21:41 -0300

    delete

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Category = require("./Category");
 const slugify = require("slugify");
+const res = require("express/lib/response");
 
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new")
@@ -23,7 +24,44 @@ router.post("/categories/save", (req, res) => {
 });
 
 router.get("/admin/categories", (req, res) => {
-    res.render("admin/categories/index");
+    Category.findAll().then(categories => {
+    res.render("admin/categories/index", {categories: categories});
+    });
+});
+
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            });
+        }else{ //Não for um número
+            res.redirect("/admin/categories");
+        }
+    }else{//Null
+        res.redirect("/admin/categories");
+    }
+});
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;
+    if(isNaN(id)){
+        res.redirect("/admin/categories");
+    }
+    Category.findByPk(id).then(category => {
+        if(category != undefined){
+            res.render("admin/categories/edit", {category: category});
+        }else{
+            res.redirect("/admin/categories");
+        }
+    }).catch(error => {
+        res.redirect("/admin/categories");
+    })
 });
 
 module.exports = router;
