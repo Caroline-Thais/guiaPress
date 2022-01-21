dfab23a45953be84e3a30a907f9d7dbe5a1af501
Thaís Silva <thaiscaroline.cms@gmail.com>
2022-01-21 13:40:02 -0300

    saving articles at db and adjusting Sequelize timezone'

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../categories/Category")
+const Article = require("./Article");
+const slugify = require("slugify");
 
-router.get("/articles", (req, res) => {
+router.get("/admin/articles", (req, res) => {
     res.send("Rota de Artigos")
 });
 
@@ -11,6 +13,21 @@ router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then(categories => {
         res.render("admin/articles/new", {categories: categories})
     })    
+});
+
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles");
+    });
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize('guiapress', 'root', 'Passei1t',{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
 });
 
 module.exports = connection;
