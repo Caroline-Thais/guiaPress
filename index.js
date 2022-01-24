7f2fd07bd51795c29caa85efadeacf5e06e8d798
Thaís Silva <thaiscaroline.cms@gmail.com>
2022-01-24 18:46:33 -0300

    pagination and start login system

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const connection = require("./database/database");
 
 const categoriesController = require("./categories/CategoriesController");
 const articlesController = require("./articles/ArticlesController");
+const usersController = require("./users/UsersController");
 
 const Article = require("./articles/Article");
 const Category = require("./categories/Category");
+const User = require("./users/User");
 
 //View engine
 app.set("view engine", "ejs");
@@ -28,14 +30,17 @@ connection
     }).catch((error) => {
         console.log(error);
     });
+
 app.use("/", categoriesController);
 app.use("/", articlesController);
+app.use("/", usersController);
 
 app.get("/", (req, res) => {
     Article.findAll({
         order: [
             ['id','DESC']
-        ]
+        ],
+        limit: 4
     }).then(articles => { 
         Category.findAll().then(categories => {
         res.render("index", {articles: articles, categories: categories});
