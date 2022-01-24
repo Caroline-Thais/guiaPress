876221fbfbedfd5f68cb9591274d795e8e3f71cf
Thaís Silva <thaiscaroline.cms@gmail.com>
2022-01-24 10:20:03 -0300

    edit article

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Category = require("../categories/Category")
 const Article = require("./Article");
 const slugify = require("slugify");
+const res = require("express/lib/response");
 
 router.get("/admin/articles", (req, res) => {
     Article.findAll({
@@ -51,6 +52,38 @@ router.post("/articles/delete", (req, res) => {
     }else{//Null
         res.redirect("/admin/categories");
     }
+});
+
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/articles");
+    }
+
+    Article.findByPk(id).then(article => {
+        if(article != undefined){
+            res.render("admin/articles/edit", {article: article});
+        }else{
+            res.redirect("/admin/articles");
+        }
+    }).catch(error => {
+        res.redirect("/admin/articles");
+    })
+});
+
+router.post("/articles/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+
+    Article.update({title: title, body: body, slug: slugify(title)}, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    })
 });
 
 module.exports = router;
