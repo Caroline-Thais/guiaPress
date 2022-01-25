a0f9225a89bb3a747e98834209c20f10a5da6c74
Thaís Silva <thaiscaroline.cms@gmail.com>
2022-01-25 11:44:59 -0300

    login

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
+const bcrypt = require("bcryptjs");
 
 router.get("/admin/users", (req, res) => {
-    res.send("Listagem de usuários");
+    User.findAll().then(users => {
+    res.render("admin/users/index", {users: users});
+    });
 });
 
 router.get("/admin/users/create", (req, res) => {
@@ -13,6 +16,31 @@ router.get("/admin/users/create", (req, res) => {
 router.post("/users/create", (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
-})
+
+    User.findOne({
+        where: {
+            email: email
+        }
+    }).then(user => {
+        if (user == undefined){
+
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+
+    User.create({
+        email: email,
+        password: hash
+    }).then(() => {
+        res.redirect("/");
+    }).catch((err) => {
+        res.redirect("/");
+    });
+
+    
+    }else{
+            res.redirect("/admin/users/create");
+        }
+    });
+});
 
 module.exports = router;
